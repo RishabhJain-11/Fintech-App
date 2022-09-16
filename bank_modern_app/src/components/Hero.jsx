0ccdd2386e8480c8f19cee97273c124860be41c9
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-16 22:15:09 +0530

    ..

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -24,6 +24,9 @@ const Hero = () => {
           </div>
         </div>
 
+
+        
+
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
           Payment Method.
         </h1>
